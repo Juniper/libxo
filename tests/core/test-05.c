0c03ba15fe82a7ee825c66aae1282bb1a7155a92
Phil Shafer <phil@juise.org>
2014-07-13 23:32:58 -0400

    test data

---
+++ tests/core/test-05.c
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2014, Juniper Networks, Inc.
+ * All rights reserved.
+ * This SOFTWARE is licensed under the LICENSE provided in the
+ * ../Copyright file. By downloading, installing, copying, or otherwise
+ * using the SOFTWARE, you agree to be bound by the terms of that
+ * LICENSE.
+ * Phil Shafer, July 2014
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "libxo.h"
+
+xo_info_t info[] = {
+    { "employee", "object", "Employee data" },
+    { "first-name", "string", "First name of employee" },
+    { "last-name", "string", "Last name of employee" },
+    { "department", "number", "Department number" },
+    { "percent-time", "number", "Percentage of full & part time (%)" },
+};
+int info_count = (sizeof(info) / sizeof(info[0]));
+
+int
+main (int argc, char **argv)
+{
+    struct employee {
+	const char *e_first;
+	const char *e_last;
+	unsigned e_dept;
+	unsigned e_percent;
+    } employees[] = {
+	{ "Terry", "Jones", 660, 90 },
+	{ "Leslie", "Patterson", 341,60 },
+	{ "Ashley", "Meter & Smith", 1440, 40 },
+	{ NULL, NULL }
+    }, *ep = employees;
+
+    xo_set_info(NULL, info, info_count);
+
+    xo_open_container("employees");
+    xo_open_list("employee");
+
+    xo_emit("{T:Last Name/%-12s}{T:First Name/%-14s}"
+	    "{T:Department/%12s}{T:Time (%)\n");
+    for ( ; ep->e_first; ep++) {
+	xo_open_instance("employee");
+	xo_emit("{:first-name/%-12s/%s}{:last-name/%-14s/%s}"
+		"{:department/%8u/%u}{:percent-time/%8u/%u}\n",
+		ep->e_first, ep->e_last, ep->e_dept, ep->e_percent);
+	xo_close_instance("employee");
+    }
+
+    xo_close_list("employee");
+    xo_close_container("employees");
+
+    return 0;
+}
