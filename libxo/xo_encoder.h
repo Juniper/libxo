f167e91024c3ee7d121cb046114411c62aef6283
Phil Shafer <phil@juise.org>
2015-08-04 17:35:53 -0400

    Add +xo_encoder_op_name and xo_encoder_setup

--- libxo/xo_encoder.h
+++ libxo/xo_encoder.h
@@ -28,16 +28,22 @@ typedef unsigned xo_encoder_op_t;
 
 /* Encoder operations */
 #define XO_OP_UNKNOWN		0
-#define XO_OP_CREATE		1
+#define XO_OP_CREATE		1 /* Called when the handle is init'd */
 #define XO_OP_OPEN_CONTAINER	2
 #define XO_OP_CLOSE_CONTAINER	3
 #define XO_OP_OPEN_LIST		4
 #define XO_OP_CLOSE_LIST	5
 #define XO_OP_OPEN_LEAF_LIST	6
 #define XO_OP_CLOSE_LEAF_LIST	7
-#define XO_OP_STRING		8 /* Quoted UTF-8 string */
-#define XO_OP_CONTENT		9 /* Other content */
-#define XO_OP_DESTROY		10 /* Clean up function */
+#define XO_OP_OPEN_INSTANCE	8
+#define XO_OP_CLOSE_INSTANCE	9
+#define XO_OP_STRING		10 /* Quoted UTF-8 string */
+#define XO_OP_CONTENT		11 /* Other content */
+#define XO_OP_FLUSH		12 /* Clean up function */
+#define XO_OP_FINISH		13 /* Clean up function */
+#define XO_OP_DESTROY		14 /* Clean up function */
+#define XO_OP_ATTR		15 /* Attribute name/value */
+#define XO_OP_VERSION		16 /* Version string */
 
 #define XO_ENCODER_HANDLER_ARGS					\
 	xo_handle_t *xop __attribute__ ((__unused__)),		\
@@ -65,7 +71,9 @@ typedef int (*xo_encoder_init_func_t)(XO_ENCODER_INIT_ARGS);
  * should return zero for success.
  */
 #define XO_ENCODER_INIT_NAME_TOKEN xo_encoder_library_init
-#define XO_ENCODER_INIT_NAME "" ## XO_ENCODER_INIT_NAME_TOKEN
+#define XO_STRINGIFY(_x) #_x
+#define XO_STRINGIFY2(_x) XO_STRINGIFY(_x)
+#define XO_ENCODER_INIT_NAME XO_STRINGIFY2(XO_ENCODER_INIT_NAME_TOKEN)
 extern int XO_ENCODER_INIT_NAME_TOKEN (XO_ENCODER_INIT_ARGS);
 
 void
@@ -101,5 +109,8 @@ xo_encoder_handle (xo_handle_t *xop, xo_encoder_op_t op,
 
 void
 xo_encoders_clean (void);
+
+const char *
+xo_encoder_op_name (xo_encoder_op_t op);
 
 #endif /* XO_ENCODER_H */
