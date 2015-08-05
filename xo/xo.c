58ddc520f657e50b09f9fc564ecea5faf42d4245
Phil Shafer <phil@juise.org>
2015-08-05 12:15:50 -0400

    rename xo*.h to xo_*.h for conssitency

--- xo/xo.c
+++ xo/xo.c
@@ -13,9 +13,9 @@
 #include <stdarg.h>
 #include <string.h>
 
-#include "xoconfig.h"
+#include "xo_config.h"
 #include "xo.h"
-#include "xoversion.h"
+#include "xo_version.h"
 
 #include <getopt.h>		/* Include after xo.h for testing */
 
