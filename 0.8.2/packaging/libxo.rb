951486f0b5337f10133d7fbf5ae8363bd1e56063
Phil Shafer <phil@juise.org>
2017-06-14 13:36:23 -0400

    new packaging data

---
+++ 0.8.2/packaging/libxo.rb
@@ -0,0 +1,20 @@
+#
+# Homebrew formula file for libxo
+# https://github.com/mxcl/homebrew
+#
+
+require 'formula'
+
+class Libxo < Formula
+  homepage 'https://github.com/Juniper/libxo'
+  url 'https://github.com/Juniper/libxo/releases/download/0.8.2/libxo-0.8.2.tar.gz'
+  sha1 'b88f7bc1af76070ade33dd1dbfe18d2f45d850c3'
+
+  depends_on 'libtool' => :build
+
+  def install
+    system "./configure", "--disable-dependency-tracking", "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+end
