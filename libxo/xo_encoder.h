86e41dfb9f4fd530bb1fd5f79688f58ae7bff2a2
Phil Shafer <phil@juise.org>
2019-10-27 15:53:28 -0400

    use xo_streq

--- libxo/xo_encoder.h
+++ libxo/xo_encoder.h
@@ -18,11 +18,23 @@
 #ifndef XO_ENCODER_H
 #define XO_ENCODER_H
 
+#include <string.h>
+
 /*
  * Expose libxo's memory allocation functions
  */
 extern xo_realloc_func_t xo_realloc;
 extern xo_free_func_t xo_free;
+
+/*
+ * Simple string comparison function (without the temptation
+ * to forget the "== 0").
+ */
+static inline int
+xo_streq (const char *one, const char *two)
+{
+    return strcmp(one, two) == 0;
+}
 
 /* Flags for formatting functions */
 typedef unsigned long xo_xff_flags_t;
