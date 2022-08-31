62ca333e74eddad7848c92cfce1a841f8546b789
Phil Shafer <phil@juise.org>
2022-08-31 15:52:36 -0400

    Implement the map and map-file options; fix bug with XO_FAILURE_NAME only used if in DTRT mode

--- libxo/libxo.c
+++ libxo/libxo.c
@@ -141,6 +141,17 @@ static const char xo_default_format[] = "%s";
 #define UNUSED __attribute__ ((__unused__))
 #endif /* UNUSED */
 
+#ifndef LIBXO_TEXT_ONLY
+/* We don't want the overhead of tag maps when in text-only mode */
+#define LIBXO_NEED_MAP
+
+#define XO_MAP_INCR 128		/* Must be even */
+
+#ifndef XO_MAPDIR
+#define XO_MAPDIR XO_SHAREDIR "/map"
+#endif /* XO_MAPDIR */
+#endif /* LIBXO_TEXT_ONLY */
+
 #define XO_INDENT_BY 2	/* Amount to indent when pretty printing */
 #define XO_DEPTH	128	 /* Default stack depth */
 #define XO_MAX_ANCHOR_WIDTH (8*1024) /* Anything wider is just silly */
@@ -273,6 +284,12 @@ struct xo_handle_s {
     char *xo_gt_domain;		/* Gettext domain, suitable for dgettext(3) */
     xo_encoder_func_t xo_encoder; /* Encoding function */
     void *xo_private;		/* Private data for external encoders */
+#ifdef LIBXO_NEED_MAP
+    char **xo_map;		/* Name mapping array */
+    int xo_map_size;		/* Size (count) of xo_map[] */
+    int xo_map_len;		/* Current length (count) of xo_map[] */
+    xo_buffer_t xo_map_data;	/* Data values for name mapping */
+#endif /* LIBXO_NEED_MAP */
 };
 
 /* Flag operations */
@@ -428,6 +445,9 @@ xo_buf_append_div (xo_handle_t *xop, const char *class, xo_xff_flags_t flags,
 static void
 xo_anchor_clear (xo_handle_t *xop);
 
+static int
+xo_map_option (xo_handle_t *xop, const char *opts);
+
 /*
  * xo_style is used to retrieve the current style.  When we're built
  * for "text only" mode, we use this function to drive the removal
@@ -582,7 +602,7 @@ xo_no_setlocale (void)
 static const char *
 xo_xml_leader_len (xo_handle_t *xop, const char *name, xo_ssize_t nlen)
 {
-    if (name == NULL || isalpha(name[0]) || name[0] == '_')
+    if (name == NULL || name[0] == '\0' || isalpha(name[0]) || name[0] == '_')
         return "";
 
     xo_failure(xop, "invalid XML tag name: '%.*s'", nlen, name);
@@ -2098,14 +2118,14 @@ xo_style_is_encoding (xo_handle_t *xop)
     return 0;
 }
 
-/* Simple name-value mapping */
-typedef struct xo_mapping_s {
+/* Simple name->value mapping */
+typedef struct xo_flag_mapping_s {
     xo_xff_flags_t xm_value;	/* Flag value */
     const char *xm_name;	/* String name */
-} xo_mapping_t;
+} xo_flag_mapping_t;
 
 static xo_xff_flags_t
-xo_name_lookup (xo_mapping_t *map, const char *value, ssize_t len)
+xo_name_lookup (xo_flag_mapping_t *map, const char *value, ssize_t len)
 {
     if (len == 0)
 	return 0;
@@ -2133,7 +2153,7 @@ xo_name_lookup (xo_mapping_t *map, const char *value, ssize_t len)
 
 #ifdef NOT_NEEDED_YET
 static const char *
-xo_value_lookup (xo_mapping_t *map, xo_xff_flags_t value)
+xo_value_lookup (xo_flag_mapping_t *map, xo_xff_flags_t value)
 {
     if (value == 0)
 	return NULL;
@@ -2146,7 +2166,7 @@ xo_value_lookup (xo_mapping_t *map, xo_xff_flags_t value)
 }
 #endif /* NOT_NEEDED_YET */
 
-static xo_mapping_t xo_xof_names[] = {
+static xo_flag_mapping_t xo_xof_names[] = {
     { XOF_COLOR_ALLOWED, "color" },
     { XOF_COLOR, "color-force" },
     { XOF_COLUMNS, "columns" },
@@ -2175,7 +2195,7 @@ static xo_mapping_t xo_xof_names[] = {
 };
 
 /* Options available via the environment variable ($LIBXO_OPTIONS) */
-static xo_mapping_t xo_xof_simple_names[] = {
+static xo_flag_mapping_t xo_xof_simple_names[] = {
     { XOF_COLOR_ALLOWED, "color" },
     { XOF_FLUSH, "flush" },
     { XOF_FLUSH_LINE, "flush-line" },
@@ -2319,7 +2339,7 @@ xo_set_options_simple (xo_handle_t *xop, const char *input)
 int
 xo_set_options (xo_handle_t *xop, const char *input)
 {
-    char *cp, *ep, *vp, *np, *bp;
+    char *cp, *ep, *vp, *np, *bp, *zp;
     int style = -1, new_style, rc = 0;
     ssize_t len;
     xo_xof_flags_t new_flag;
@@ -2448,7 +2468,11 @@ xo_set_options (xo_handle_t *xop, const char *input)
 	    continue;
 	}
 
+	/* We allow either '=' or ':' to separate the keyword from the value */
 	vp = strchr(cp, '=');
+	zp = strchr(cp, ':');
+	if (zp != NULL && (vp == NULL || zp < vp))
+	    vp = zp;
 	if (vp)
 	    *vp++ = '\0';
 
@@ -2487,6 +2511,24 @@ xo_set_options (xo_handle_t *xop, const char *input)
 			xo_warnx("error initializing encoder: %s", vp);
 		}
 		
+	    } else if (xo_streq(cp, "map")) {
+		if (vp == NULL)
+		    xo_failure(xop, "missing value for map option");
+		else {
+		    rc = xo_map_option(xop, vp);
+		    if (rc)
+			xo_warnx("error initializing map: '%s'", vp);
+		}
+
+	    } else if (xo_streq(cp, "map-file")) {
+		if (vp == NULL)
+		    xo_failure(xop, "missing value for map-file option");
+		else {
+		    rc = xo_map_add_file(xop, vp);
+		    if (rc)
+			xo_warnx("error initializing map-file: '%s'", vp);
+		}
+
 	    } else {
 		xo_warnx("unknown libxo option value: '%s'", cp);
 		rc = -1;
@@ -4366,6 +4408,226 @@ xo_arg (xo_handle_t *xop)
 }
 #endif /* 0 */
 
+/*
+ * We want to allow mapping from one "name" to another replacement
+ * name, like "df --libxo mapfile=df.map", so we can change the
+ * vocabulary as needed.  This means maintaining an array of old and
+ * new names, along with the memory for the strings themselves.  We
+ * want this to be specific to the handle, so when the user requests a
+ * map, we don't break other users of libxo.  We'll need two fields in
+ * the handle, one for the array, and one for the string buffer.
+ */
+#ifdef LIBXO_NEED_MAP
+static int
+xo_map_find (xo_handle_t *xop, const char *name, size_t len)
+{
+    for (int i = 0; i < xop->xo_map_len; i += 2) {
+	if (strncmp(xop->xo_map[i], name, len) == 0)
+	    return i;
+    }
+
+    return -1;
+}
+
+/*
+ * Is the path something we can find in $XO_MAPDIR?  The current test
+ * is simple: lack of '/'.
+ */
+static int
+xo_is_shareable_filename (const char *path)
+{
+    return strchr(path, '/') == NULL;
+}
+
+#endif /* LIBXO_NEED_MAP */
+
+/*
+ * Find the replacement string for a tag, or return the tag itself
+ */
+static inline const char *
+xo_map_name (xo_handle_t *xop UNUSED, const char *name)
+{
+#ifdef LIBXO_NEED_MAP
+    if (name == NULL)
+	return NULL;
+
+    size_t len = strlen(name);
+    for (int i = 0; i < xop->xo_map_len; i += 2) {
+	if (strncmp(xop->xo_map[i], name, len) == 0)
+	    return xop->xo_map[i + 1];
+    }
+#endif /* LIBXO_NEED_MAP */
+
+    return name;
+}
+
+/*
+ * Add a mapping from one tag name to another.  Both "from" and "to" are
+ * UTF-8 strings.
+ */
+int
+xo_map_add (xo_handle_t *xop UNUSED, const char *from UNUSED,
+	    size_t flen UNUSED, const char *to UNUSED, size_t tlen UNUSED)
+{
+#ifdef LIBXO_NEED_MAP
+    xop = xo_default(xop);
+
+    int val = xo_map_find(xop, from, flen);
+    if (val >= 0) {
+	/* We hit a "from" value that's already there; replace the "to" */
+	char *newp = xo_buf_append_val(&xop->xo_map_data, to, tlen);
+	if (newp == NULL)
+	    return -1;
+
+	/* NUL terminate the string */
+	if (!xo_buf_append_val(&xop->xo_map_data, "", 1))
+	    return -1;
+
+	xop->xo_map[val + 1] = newp;
+
+	return 0;
+    }
+
+    if (xop->xo_map_len >= xop->xo_map_size) {
+	char **newp = xo_realloc(xop->xo_map, xop->xo_map_size + XO_MAP_INCR);
+	if (newp == NULL)
+	    return -1;
+	xop->xo_map = newp;
+	xop->xo_map_size += XO_MAP_INCR;
+    }
+
+    char *new_from = xo_buf_append_val(&xop->xo_map_data, from, flen);
+    if (new_from == NULL)
+	return -1;
+
+    /* NUL terminate the new string */
+    if (!xo_buf_append_val(&xop->xo_map_data, "", 1))
+	return -1;
+
+    char *new_to = xo_buf_append_val(&xop->xo_map_data, to, tlen);
+    if (new_to == NULL)
+	return -1;
+
+    /* NUL terminate the new string */
+    if (!xo_buf_append_val(&xop->xo_map_data, "", 1))
+	return -1;
+
+    val = xop->xo_map_len;	/* Use next slot */
+
+    xop->xo_map[val] = new_from;
+    xop->xo_map[val + 1] = new_to;
+
+    xop->xo_map_len += 2;	/* Consume the slot */
+#endif /* LIBXO_NEED_MAP */
+
+    return 0;
+}
+
+static int
+xo_map_option (xo_handle_t *xop, const char *opts)
+{
+    const char *cp, *np, *vp, *ep;
+    size_t nlen, vlen;
+
+    for (np = opts; *np; np = ep) {
+	cp = strchr(np, '=');
+	if (cp == NULL)
+	    break;
+
+	nlen = cp - np;
+
+	vp = cp + 1;		/* Skip '=' */
+	ep = strchr(vp, ':');
+	vlen = ep ? ep - vp : strlen(vp);
+
+	if (xo_map_add(xop, np, nlen, vp, vlen))
+	    return -1;
+
+	if (ep == NULL)
+	    break;
+	ep += 1;		/* Skip ':' */
+    }
+
+    return 0;
+}
+
+/*
+ * Add a file of tag maps, with the format:
+ *    # example comment
+ *    old-tag=new-tag
+ *    ancient=new-hotness
+ * The file should be UTF-8.
+ */
+int
+xo_map_add_file (xo_handle_t *xop UNUSED, const char *fname UNUSED)
+{
+#ifdef LIBXO_NEED_MAP
+    const char bom0 = 0xEF, bom1 = 0xBB, bom2 = 0xBF;
+
+    char buf[BUFSIZ], *np, *cp, *ep, *vp;
+    int first = TRUE;
+
+    xop = xo_default(xop);
+
+    FILE *fp = fopen(fname, "r");
+    if (fp == NULL) {
+	if (!xo_is_shareable_filename(fname))
+	    return -1;
+
+	static const char dir[] = XO_MAPDIR;
+	size_t dlen = sizeof(dir) - 1;
+	size_t flen = strlen(fname);
+	char *new_path = alloca(dlen + 1 + flen + 1);
+	memcpy(new_path, dir, dlen);
+        new_path[dlen] = '/';
+	memcpy(new_path + dlen + 1, fname, flen);
+	new_path[dlen + 1 + flen] = '\0';
+
+	fp = fopen(new_path, "r");
+	if (fp == NULL)
+	    return -1;
+    }
+
+    for (;;) {
+	if (fgets(buf, sizeof(buf), fp) == NULL)
+	    break;
+
+	/*
+	 * The UTF-8 file can start with a "BOM":
+	 *    https://en.wikipedia.org/wiki/Byte_order_mark
+	 * So if we see this at the start of the file, we need to skip
+	 * over it.
+	 */
+	cp = buf;
+	if (first && buf[0] == bom0 && buf[1] == bom1 && buf[2] == bom2)
+	    cp += 3;
+	first = FALSE;
+
+	/* Skip to the start of the name (the "from") */
+	np = cp + strspn(cp, " \t\r\n");
+	if (*np == '#')
+	    continue;
+
+	/* Skip to the "=" */
+	cp = np + strcspn(np, " \t\r\n=");
+	if (cp == np)
+	    continue;
+
+	/* Find the start and the end of the value (the "to") */
+	vp = cp + 1 + strspn(cp + 1, " \t\r\n=");
+	ep = vp + strcspn(vp, " \t\r\n;");
+	if (ep == vp)
+	    continue;
+
+	(void) xo_map_add(xop, np, cp - np, vp, ep - vp);
+    }
+
+    fclose(fp);
+#endif /* LIBXO_NEED_MAP */
+
+    return 0;
+}
+
 static void
 xo_format_value (xo_handle_t *xop, const char *name, ssize_t nlen,
 		 const char *value, ssize_t vlen,
@@ -4446,6 +4708,20 @@ xo_format_value (xo_handle_t *xop, const char *name, ssize_t nlen,
 
     xo_buffer_t *xbp = &xop->xo_data;
     xo_humanize_save_t save;	/* Save values for humanizing logic */
+
+    if (name) {
+	/*
+	 * We have a name, but need to see if it's been remapped
+	 * to a different name.  To look up the tag name, we need
+	 * to make a local copy and NUL terminate it.
+	 */
+	char *new_name  = alloca(nlen + 1);
+	memcpy(new_name, name, nlen);
+	new_name[nlen] = '\0';
+
+	name = xo_map_name(xop, new_name);
+	nlen = strlen(name);	/* Need new length for new name */
+    }
 
     const char *leader = xo_xml_leader_len(xop, name, nlen);
 
@@ -5439,7 +5715,7 @@ xo_role_wants_default_format (int ftype)
     return 1;
 }
 
-static xo_mapping_t xo_role_names[] = {
+static xo_flag_mapping_t xo_role_names[] = {
     { 'C', "color" },
     { 'D', "decoration" },
     { 'E', "error" },
@@ -5459,7 +5735,7 @@ static xo_mapping_t xo_role_names[] = {
 #define XO_ROLE_TEXT	'+'
 #define XO_ROLE_NEWLINE	'\n'
 
-static xo_mapping_t xo_modifier_names[] = {
+static xo_flag_mapping_t xo_modifier_names[] = {
     { XFF_ARGUMENT, "argument" },
     { XFF_COLON, "colon" },
     { XFF_COMMA, "comma" },
@@ -5485,7 +5761,7 @@ static xo_mapping_t xo_modifier_names[] = {
 };
 
 #ifdef NOT_NEEDED_YET
-static xo_mapping_t xo_modifier_short_names[] = {
+static xo_flag_mapping_t xo_modifier_short_names[] = {
     { XFF_COLON, "c" },
     { XFF_DISPLAY_ONLY, "d" },
     { XFF_ENCODE_ONLY, "e" },
@@ -7100,6 +7376,8 @@ xo_do_open_container (xo_handle_t *xop, xo_xof_flags_t flags, const char *name)
 	name = XO_FAILURE_NAME;
     }
 
+    name = xo_map_name(xop, name); /* Find mapped name, if any */
+
     const char *leader = xo_xml_leader(xop, name);
     flags |= xop->xo_flags;	/* Pick up handle flags */
 
@@ -7208,18 +7486,22 @@ xo_do_close_container (xo_handle_t *xop, const char *name)
 	    char *cp = alloca(len);
 	    memcpy(cp, name, len);
 	    name = cp;
-	} else if (!(xsp->xs_flags & XSF_DTRT)) {
-	    xo_failure(xop, "missing name without 'dtrt' mode");
+	} else {
+	    if (!(xsp->xs_flags & XSF_DTRT))
+		xo_failure(xop, "missing name without 'dtrt' mode");
 	    name = XO_FAILURE_NAME;
 	}
     }
+
+    name = xo_map_name(xop, name); /* Find mapped name, if any */
 
     const char *leader = xo_xml_leader(xop, name);
 
     switch (xo_style(xop)) {
     case XO_STYLE_XML:
 	xo_depth_change(xop, name, -1, -1, XSS_CLOSE_CONTAINER, 0);
-	rc = xo_printf(xop, "%*s</%s%s>%s", xo_indent(xop), "", leader, name, ppn);
+	rc = xo_printf(xop, "%*s</%s%s>%s", xo_indent(xop),
+		       "", leader, name, ppn);
 	break;
 
     case XO_STYLE_JSON:
@@ -7284,6 +7566,8 @@ xo_do_open_list (xo_handle_t *xop, xo_xof_flags_t flags, const char *name)
 
     const char *ppn = XOF_ISSET(xop, XOF_PRETTY) ? "\n" : "";
     const char *pre_nl = "";
+
+    name = xo_map_name(xop, name); /* Find mapped name, if any */
 
     switch (xo_style(xop)) {
     case XO_STYLE_JSON:
@@ -7378,11 +7662,14 @@ xo_do_close_list (xo_handle_t *xop, const char *name)
 	    char *cp = alloca(len);
 	    memcpy(cp, name, len);
 	    name = cp;
-	} else if (!(xsp->xs_flags & XSF_DTRT)) {
-	    xo_failure(xop, "missing name without 'dtrt' mode");
+	} else {
+	    if (!(xsp->xs_flags & XSF_DTRT))
+		xo_failure(xop, "missing name without 'dtrt' mode");
 	    name = XO_FAILURE_NAME;
 	}
     }
+
+    name = xo_map_name(xop, name); /* Find mapped name, if any */
 
     switch (xo_style(xop)) {
     case XO_STYLE_JSON:
@@ -7444,6 +7731,8 @@ xo_do_open_leaf_list (xo_handle_t *xop, xo_xof_flags_t flags, const char *name)
     const char *ppn = XOF_ISSET(xop, XOF_PRETTY) ? "\n" : "";
     const char *pre_nl = "";
 
+    name = xo_map_name(xop, name); /* Find mapped name, if any */
+
     switch (xo_style(xop)) {
     case XO_STYLE_JSON:
 	indent = 1;
@@ -7497,11 +7786,14 @@ xo_do_close_leaf_list (xo_handle_t *xop, const char *name)
 	    char *cp = alloca(len);
 	    memcpy(cp, name, len);
 	    name = cp;
-	} else if (!(xsp->xs_flags & XSF_DTRT)) {
-	    xo_failure(xop, "missing name without 'dtrt' mode");
+	} else {
+	    if (!(xsp->xs_flags & XSF_DTRT))
+		xo_failure(xop, "missing name without 'dtrt' mode");
 	    name = XO_FAILURE_NAME;
 	}
     }
+
+    name = xo_map_name(xop, name); /* Find mapped name, if any */
 
     switch (xo_style(xop)) {
     case XO_STYLE_JSON:
@@ -7540,6 +7832,8 @@ xo_do_open_instance (xo_handle_t *xop, xo_xof_flags_t flags, const char *name)
 	xo_failure(xop, "NULL passed for instance name");
 	name = XO_FAILURE_NAME;
     }
+
+    name = xo_map_name(xop, name); /* Find mapped name, if any */
 
     const char *leader = xo_xml_leader(xop, name);
     flags |= xop->xo_flags;
@@ -7631,11 +7925,14 @@ xo_do_close_instance (xo_handle_t *xop, const char *name)
 	    char *cp = alloca(len);
 	    memcpy(cp, name, len);
 	    name = cp;
-	} else if (!(xsp->xs_flags & XSF_DTRT)) {
-	    xo_failure(xop, "missing name without 'dtrt' mode");
+	} else {
+	    if (!(xsp->xs_flags & XSF_DTRT))
+		xo_failure(xop, "missing name without 'dtrt' mode");
 	    name = XO_FAILURE_NAME;
 	}
     }
+
+    name = xo_map_name(xop, name); /* Find mapped name, if any */
 
     const char *leader = xo_xml_leader(xop, name);
 
@@ -7762,6 +8059,8 @@ xo_do_close (xo_handle_t *xop, const char *name, xo_state_t new_state)
 	need_state = XSS_MARKER;
     else
 	return 0; /* Unknown or useless new states are ignored */
+
+    name = xo_map_name(xop, name);
 
     for (xsp = &xop->xo_stack[xop->xo_depth]; xsp > xop->xo_stack; xsp--) {
 	/*
