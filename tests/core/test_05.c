cb4f17317295e04f078e5517c1d2bee4472fe8a6
Phil Shafer <phil@juise.org>
2024-03-06 22:28:26 -0500

    fix test cases

--- tests/core/test_05.c
+++ tests/core/test_05.c
@@ -123,8 +123,8 @@ main (int argc, char **argv)
 	    "{T:/%-12s}{T:Time (%)}\n", "Department");
     for ( ; ep->e_first; ep++) {
 	xo_open_instance("employee");
-	xo_emit("{[:-25}{:first-name/%s} ({:nic-name/\"%s\"}){]:}"
-		"{:last-name/%-14..14s/%s}"
+	xo_emit("{[:-25}{k:first-name/%s} ({k:nic-name/\"%s\"}){]:}"
+		"{k:last-name/%-14..14s/%s}"
 		"{:department/%8u}{:percent-time/%8u}\n",
 		ep->e_first, ep->e_nic, ep->e_last, ep->e_dept, ep->e_percent);
 	if (ep->e_percent > 50) {
