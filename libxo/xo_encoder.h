c196f75fa36e2b645a3050ff3a85988e8d52cbd7
Phil Shafer <phil@juise.org>
2024-03-06 22:27:59 -0500

    checkpoint filter work

--- libxo/xo_encoder.h
+++ libxo/xo_encoder.h
@@ -21,6 +21,7 @@
 #include <string.h>
 
 #include "xo_private.h"
+#include "xo_buf.h"
 
 /* Flags for formatting functions */
 typedef unsigned long xo_xff_flags_t;
@@ -78,25 +79,67 @@ typedef unsigned xo_encoder_op_t;
 #define XO_OP_OPTIONS		17 /* Additional command line options */
 #define XO_OP_OPTIONS_PLUS	18 /* Additional command line options */
 
-#define XO_ENCODER_HANDLER_ARGS					\
-	xo_handle_t *xop __attribute__ ((__unused__)),		\
-	xo_encoder_op_t op __attribute__ ((__unused__)),	\
-	const char *name __attribute__ ((__unused__)),		\
-        const char *value __attribute__ ((__unused__)),		\
-	void *private __attribute__ ((__unused__)),		\
-	xo_xff_flags_t flags __attribute__ ((__unused__))
+#define XO_ENCODER_HANDLER_ARGS			\
+	xo_handle_t *xop XO_UNUSED,		\
+	xo_encoder_op_t op XO_UNUSED,	        \
+	xo_buffer_t *bufp XO_UNUSED,             \
+	const char *name XO_UNUSED,		\
+        const char *value XO_UNUSED,		\
+	void *private XO_UNUSED,		\
+	xo_xff_flags_t flags XO_UNUSED
 
 typedef int (*xo_encoder_func_t)(XO_ENCODER_HANDLER_ARGS);
+
+/*
+ * White generating filtered output, we need to make "tentative"
+ * content, which might end up either as output or discarded when we
+ * decide we don't need it.  For example, if the filter is "a/b/c",
+ * then we need to make output for both "a" and "b" before we have a
+ * chance of seeing "c".
+ *
+ * The problem is exacerbated by encoders possibly needing to repeat
+ * "prefix" content for each child or nested field.  So this leading
+ * content must be preserved outside the normal output buffering.
+ *
+ * We use the "whiteboard" to allow this tentative output generation.
+ * We can create whiteboards for each actve "match" and write content
+ * into it, recording offsets for each container/list/instance using
+ * our stack.  When we pop items off the stack, the encoder can zero
+ * out the byte for each frame's offset, clearing the trailing bits of
+ * the content.
+ *
+ * If this is not sufficient, the encoder can use this offset to hold
+ * any sort of opaque data that is needed.
+ */
+
+/* Operatons for whiteboards */
+#define XO_WB_INIT		1 /* Initialize the whiteboard */
+#define XO_WB_RECORD		2 /* Record the current offset */
+#define XO_WB_ALLOW		3 /* Allow output */
+#define XO_WB_DENY		4 /* Deny output */
+#define XO_WB_CLEAN		5 /* Clean up and release */
+
+typedef unsigned xo_whiteboard_op_t;	/* Whiteboard operation */
+
+#define XO_WHITEBOARD_FUNC_ARGS              \
+    xo_handle_t *xop XO_UNUSED,		     \
+    xo_whiteboard_op_t op  XO_UNUSED,        \
+    xo_buffer_t *wbp XO_UNUSED,		     \
+    xo_off_t *offp XO_UNUSED,		     \
+    void *private XO_UNUSED
+
+typedef int (*xo_whiteboard_func_t)(XO_WHITEBOARD_FUNC_ARGS);
 
 typedef struct xo_encoder_init_args_s {
     unsigned xei_version;	   /* Current version */
     xo_encoder_func_t xei_handler; /* Encoding handler */
+    xo_whiteboard_func_t xei_wb_marker; /* Whiteboard marker function (v2) */
 } xo_encoder_init_args_t;
 
-#define XO_ENCODER_VERSION	1 /* Current version */
+#define XO_ENCODER_VERSION	2 /* Current version */
 
 #define XO_ENCODER_INIT_ARGS \
-    xo_encoder_init_args_t *arg __attribute__ ((__unused__))
+    xo_encoder_init_args_t *arg XO_UNUSED
 
 typedef int (*xo_encoder_init_func_t)(XO_ENCODER_INIT_ARGS);
 /*
@@ -128,8 +171,12 @@ xo_set_private (xo_handle_t *xop, void *opaque);
 xo_encoder_func_t
 xo_get_encoder (xo_handle_t *xop);
 
+xo_whiteboard_func_t
+xo_get_wb_marker (xo_handle_t *xop);
+
 void
-xo_set_encoder (xo_handle_t *xop, xo_encoder_func_t encoder);
+xo_set_encoder (xo_handle_t *xop, xo_encoder_func_t encoder,
+		xo_whiteboard_func_t wb_marker);
 
 int
 xo_encoder_init (xo_handle_t *xop, const char *name);
@@ -138,14 +185,21 @@ xo_handle_t *
 xo_encoder_create (const char *name, xo_xof_flags_t flags);
 
 int
-xo_encoder_handle (xo_handle_t *xop, xo_encoder_op_t op,
+xo_encoder_handle (xo_handle_t *xop, xo_encoder_op_t op, xo_buffer_t *bufp,
 		   const char *name, const char *value, xo_xff_flags_t flags);
+
+int
+xo_encoder_wb_marker (xo_handle_t *xop, xo_whiteboard_op_t op,
+		      xo_buffer_t *wbp, xo_off_t *offp);
 
 void
 xo_encoders_clean (void);
 
 const char *
 xo_encoder_op_name (xo_encoder_op_t op);
+
+const char *
+xo_whiteboard_op_name (xo_whiteboard_op_t op);
 
 /*
  * xo_failure is used to announce internal failures, when "warn" is on
