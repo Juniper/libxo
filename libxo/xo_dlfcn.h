a3907f2c02618a4551b1aecd5b4d29eff0f2278b
Phil Shafer <phil@juise.org>
2025-04-15 14:26:45 -0400

    make filtering dynamically loadable

---
+++ libxo/xo_dlfcn.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2025, Juniper Networks, Inc.
+ * All rights reserved.
+ * This SOFTWARE is licensed under the LICENSE provided in the
+ * ../Copyright file. By downloading, installing, copying, or otherwise
+ * using the SOFTWARE, you agree to be bound by the terms of that
+ * LICENSE.
+ * Phil Shafer, April 2025
+ */
+
+#ifndef XO_DLFCN_H
+#define XO_DLFCN_H
+
+
+
+#endif /* XO_DLFCN_H */
