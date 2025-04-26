2e6cee7093892881a6baa83ae9eac7770529a569
Phil Shafer <phil@juise.org>
2025-04-26 10:48:53 -0400

    change the names of xo_filter_* function exposed by libxo.c so the names don't look like they are from xo_filter.c

--- tests/xpath/xpath_01.c
+++ tests/xpath/xpath_01.c
@@ -97,7 +97,7 @@ main (int argc, char **argv)
 	xo_errx(1, "allocation of filter failed");
 
     xo_handle_t *xop = NULL;	/* Use default output handle */
-    xo_filter_data_set(xop, xfp);
+    xo_set_filter_data(xop, xfp);
     xo_xparse_data_t *xdp = xo_filter_xparse_data(xop, xfp);
 
     xo_xparse_init(xdp);
@@ -198,7 +198,7 @@ main (int argc, char **argv)
 	    if (xfp == NULL)
 		xo_errx(1, "allocation of filter failed");
 
-	    xo_filter_data_set(xop, xfp);
+	    xo_set_filter_data(xop, xfp);
 	    xdp = xo_filter_xparse_data(xop, xfp);
 
 	    xo_xparse_init(xdp);
