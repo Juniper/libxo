3b8ea0d852939970f61d6a44f1469835d70fe271
Phil Shafer <phil@juise.org>
2026-08-02 22:37:52 -0400

    fix block comment

--- libxo/libxo.c
+++ libxo/libxo.c
@@ -2144,7 +2144,9 @@ xo_failure_filter (xo_handle_t *xop, const char *fmt, ...)
     va_end(vap);
 }
 
-/* Error callback bridging xo_parse_t errors to xo_failure() */
+/*
+ * Error callback bridging xo_parse_t errors to xo_failure()
+ */
 static void
 xo_parse_fail_cb (void *data, const char *fmt, va_list vap)
 {
