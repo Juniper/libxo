63e9523eee837609d38988a3fdc90811e418c22c
Phil Shafer <phil@juise.org>
2026-08-07 02:02:07 -0400

    update test cases

--- tests/llvm-plugins/validate_01.c
+++ tests/llvm-plugins/validate_01.c
@@ -1,55 +1,86 @@
 #include <libxo/xo.h>
 
-/* tv_syntax_ok: valid format, no diagnostic */
-void tv_syntax_ok(void) { xo_emit("{:name/%s}", "alice"); }
+int
+main (int argc, char **argv)
+{
+    char name[] = "validate_01.test";  /* test trimming of xo_program */
+    argv[0] = name;
+    
+    argc = xo_parse_args(argc, argv);
+    if (argc < 0)
+	return 1;
 
-/* tv_missing_brace: unclosed field */
-void tv_missing_brace(void) { xo_emit("{:bad/", "oops"); }
+    xo_open_container("top");
 
-/* tv_count_too_few: fewer varargs than fields */
-void tv_count_too_few(void) { xo_emit("{:name/%s} {:age/%d}", "bob"); }
+    /* valid format, no diagnostic */
+    xo_emit("{:name/%s}\n", "alice");
 
-/* tv_count_too_many: more varargs than fields */
-void tv_count_too_many(void) { xo_emit("{:name/%s}", "carol", 99); }
+    /* unclosed field */
+    xo_emit("{:bad/", "oops");
 
-/* tv_type_int_string: %d field gets string arg */
-void tv_type_int_string(void) { xo_emit("{:count/%d}", "oops"); }
+    /* fewer varargs than fields */
+    xo_emit("{:name/%s} {:age/%d}\n", "bob");
 
-/* tv_type_string_int: %s field gets int arg */
-void tv_type_string_int(void) { xo_emit("{:name/%s}", 42); }
+    /* more varargs than fields */
+    xo_emit("{:name/%s}\n", "carol", 99);
 
-/* tv_multiple_roles: multiple role characters */
-void tv_multiple_roles(void) { xo_emit("{LT:Max}"); }
+    /* %d field gets string arg */
+    xo_emit("{:count/%d}\n", "oops");
 
-/* tv_name_no_name: empty field name */
-void tv_name_no_name(void) { xo_emit("{:/%s}", "x"); }
+    /* %s field gets int arg */
+    xo_emit("{:name/%s}\n", 42);
 
-/* tv_name_underscore: underscore in field name */
-void tv_name_underscore(void) { xo_emit("{:no_good/%s}", "x"); }
+    /* multiple role characters */
+    xo_emit("{LT:Max}\n");
 
-/* tv_name_uppercase: uppercase in field name */
-void tv_name_uppercase(void) { xo_emit("{:NAME/%s}", "x"); }
+    /* empty field name */
+    xo_emit("{:/%s}\n", "x");
 
-/* tv_name_digit: name starting with digit */
-void tv_name_digit(void) { xo_emit("{:9lives/%s}", "x"); }
+    /* underscore in field name */
+    xo_emit("{:no_good/%s}\n", "x");
 
-/* tv_name_invalid_char: invalid character in field name */
-void tv_name_invalid_char(void) { xo_emit("{:a$b/%s}", "x"); }
+    /* uppercase in field name */
+    xo_emit("{:NAME/%s}\n", "x");
 
-/* tv_name_too_short: field name too short */
-void tv_name_too_short(void) { xo_emit("{:ab/%s}", "x"); }
+    /* name starting with digit */
+    xo_emit("{:9lives/%s}\n", "x");
 
-/* tv_anchor_nonnumeric: non-numeric anchor content */
-void tv_anchor_nonnumeric(void) { xo_emit("{[:mumble}{:name/%s}{]:}", "x"); }
+    /* invalid character in field name */
+    xo_emit("{:a$b/%s}\n", "x");
 
-/* tv_anchor_bad_format: anchor with non-%d format */
-void tv_anchor_bad_format(void) { xo_emit("{[:/%s}{:name/%s}{]:}", "32", "x"); }
+    /* field name too short */
+    xo_emit("{:ab/%s}\n", "x");
 
-/* tv_anchor_both: anchor with both content and format */
-void tv_anchor_both(void) { xo_emit("{[:32/%d}{:name/%s}{]:}", 32, "x"); }
+    /* non-numeric anchor content */
+    xo_emit("{[:mumble}{:name/%s}{]:}\n", "x");
 
-/* tv_humanize_no_format: humanize modifier without format */
-void tv_humanize_no_format(void) { xo_emit("{h:size}", 1024); }
+    /* anchor with non-%d format */
+    xo_emit("{[:/%s}{:name/%s}{]:}\n", "32", "x");
 
-/* tv_long_name_bad: role name too long */
-void tv_long_name_bad(void) { xo_emit("{,humanization:size/%d}", 1024); }
+    /* anchor with both content and format */
+    xo_emit("{[:32/%d}{:name/%s}{]:}\n", 32, "x");
+
+    /* humanize modifier without format */
+    xo_emit("{h:size}\n", 1024);
+
+    /* role name too long */
+    xo_emit("{,humanization:size/%d}\n", 1024);
+
+    /* wrong argument types */
+    xo_emit("{:name/%d}\n", "foo");
+    xo_emit("{:count/%s}\n", 37);
+    xo_emit("{:default}\n", 39);
+    xo_emit("{:name/%d}, {:name/%s}\n", "foo", 37);
+    xo_emit("{:value/%d.%u}, {:name/%.*s}\n", "foo", -4.537, "green", 13579);
+
+    uint8_t u8 = 8;
+    uint32_t u32 = 32;
+    uint64_t u64 = 64;
+    xo_emit("{:types/%c-%ld-%lld}\n", u64, u32, u8);
+
+    xo_close_container("top");
+
+    xo_finish();
+
+    exit(0);
+}
