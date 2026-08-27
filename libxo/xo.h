db74e7a10e7db07ab28a9ee020caeff5475ac72b
Phil Shafer <phil@juise.org>
2026-08-27 10:59:24 -0400

    Add SPDX id

--- libxo/xo.h
+++ libxo/xo.h
@@ -1,4 +1,5 @@
 /*
+ * SPDX-License-Identifier: BSD-2-Clause
  * Copyright (c) 2014-2018, Juniper Networks, Inc.
  * All rights reserved.
  * This SOFTWARE is licensed under the LICENSE provided in the
