3b2639135bb0af98fbcd4689da2267dc7b21b5fc
Phil Shafer <phil@juise.org>
2026-09-04 21:35:01 -0400

    add xf_extflags

--- llvm-plugins/validate/xo_parse_shim.c
+++ llvm-plugins/validate/xo_parse_shim.c
@@ -95,9 +95,10 @@ _Static_assert(offsetof(xo_field_info_t, xfi_num_fspecs) == 40 + sizeof(void *),
  *   offset 24: uint16_t xf_len
  *   offset 26: uint16_t xf_prefix_len
  *   offset 28: uint16_t xf_num_bits, padding
- *   total: 30 bytes
+ *   offset 30: uint32_t xf_extflags
+ *   total: 34 bytes
  */
-_Static_assert(sizeof(xo_fspec_t) == 30,
+_Static_assert(sizeof(xo_fspec_t) == 36,
 	       "xo_fspec_t size mismatch; update xo_precompile.cc FspecTy");
 _Static_assert(offsetof(xo_fspec_t, xf_fc)           ==  0, "xf_fc offset");
 _Static_assert(offsetof(xo_fspec_t, xf_lflag)        ==  1, "xf_lflag offset");
@@ -118,6 +119,7 @@ _Static_assert(offsetof(xo_fspec_t, xf_start)        == 22, "xf_start offset");
 _Static_assert(offsetof(xo_fspec_t, xf_len)          == 24, "xf_len offset");
 _Static_assert(offsetof(xo_fspec_t, xf_prefix_len)   == 26, "xf_prefix_len offset");
 _Static_assert(offsetof(xo_fspec_t, xf_num_bits  )   == 28, "xf_num_bits offset");
+_Static_assert(offsetof(xo_fspec_t, xf_extflags  )   == 32, "xf_extflags offset");
 
 struct xo_shim_state {
     xo_shim_error_t error;
@@ -524,6 +526,7 @@ xo_shim_parse_fields (const char *fmt,
             sf.xsp_prefix_len   = xfp->xf_prefix_len;
             sf.xsp_num_bits     = xfp->xf_num_bits;
             sf.xsp_padding      = xfp->xf_padding;
+            sf.xsp_extflags      = xfp->xf_extflags;
             fspec_cb(fspec_data, &sf);
         }
     }
