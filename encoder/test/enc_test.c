25349bb76549e4af68306adc55a1434346e6f558
Phil Shafer <phil@juise.org>
2026-09-17 22:39:24 -0400

    add encoder version check/fail

--- encoder/test/enc_test.c
+++ encoder/test/enc_test.c
@@ -47,6 +47,12 @@ test_handler (XO_ENCODER_HANDLER_ARGS)
 int
 xo_encoder_library_init (XO_ENCODER_INIT_ARGS)
 {
+    /* Caller's version is older than we need; report ours and fail */
+    if (arg->xei_version < XO_ENCODER_VERSION) {
+	arg->xei_version = XO_ENCODER_VERSION;
+	return -1;
+    }
+
     arg->xei_version = XO_ENCODER_VERSION;
     arg->xei_handler = test_handler;
 
