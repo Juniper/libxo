7433262810401ec1e1841ca39563c50278c7df5c
Phil Shafer <phil@juise.org>
2026-09-17 20:41:35 -0400

    make dense output

--- tests/core/test_04.c
+++ tests/core/test_04.c
@@ -44,7 +44,7 @@ main (int argc, char **argv)
     xo_set_info(NULL, info, info_count);
 
     xo_open_container("employees");
-    xo_open_list("employee");
+    xo_open_list_hf(NULL, XOF_DENSE, "employee");
 
     xo_emit("{T:Last Name/%-12s}{T:First Name/%-14s}{T:Department/%s}\n");
     for ( ; ep->e_first; ep++) {
